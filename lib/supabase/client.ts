8c174cd0799c544d8b49c284e2208c66544826bd
ethdomperin <eth.domperin91234@gmail.com>
2025-05-17 10:04:27 +0300

    Updated client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    "https://sbiorlvgotonmveplvpr.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiaW9ybHZnb3Rvbm12ZXBsdnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxOTU5NzksImV4cCI6MjA2Mjc3MTk3OX0.oiptMTmOdrCCpLP-fifdP7yYrdhZteOYoaHMUMi6BR8"
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
